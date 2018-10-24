24050b77253f0f92fa025c9b5b3d765f180939c5
Philip Weiss <weissp68@stanford.edu>
2018-10-24 16:10:52 -0700

    [skeleton] add basics of countMinSketch class

---
+++ lib/CountMinSketch.py
@@ -0,0 +1,20 @@
+
+"""
+General Count-Min-Sketch class.
+"""
+class CountMinSketch:
+    def __init__(self, n, d):
+        pass
+
+    """
+    Records an item being streamed into the CountMinSketch.
+    """
+    def record(self, item):
+        pass
+
+
+    """
+    Estimates the frequency of item. 
+    """
+    def estimate(self, item):
+        pass
---
+++ test/CountMinSketchTest.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '../lib')
+
+import CountMinSketch
+
+CountMinSketch()
